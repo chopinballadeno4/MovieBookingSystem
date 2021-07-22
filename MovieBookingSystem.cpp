002908296afec12f0af221aefb0f52982dc64b68
BaeSungMin <qo98333@naver.com>
2021-07-22 18:37:45 +0900

    로그인틀 구현 세부 메소드 구현필요

--- MovieBookingSystem.cpp
+++ MovieBookingSystem.cpp
@@ -1,20 +1,75 @@
-﻿// MovieBookingSystem.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
+﻿//#define endl '\n' 왜 오류가 나오는지 잘 모르겠음
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <stdio.h>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+void makeId() {
+
+}
+
+void findId() {
+
+}
+
+boolean inputId(string id, int pw) {
+    return false;
+}
+
+void login() {
+    int selectnum;
+    string id;
+    int pw=0;
+    cout << "===============================================" << '\n';
+    cout << "=   1. 로그인   2. ID/PW 찾기   3. 회원가입   =" << '\n';
+    cout << "===============================================" << '\n';
+    cout << "번호 입력: ";
+    cin >> selectnum;
+    cout << 'k';
+
+    if (selectnum == 1) {
+        cout << '\n';
+        cout << '\n';
+        cout << '\n';
+        if (inputId(id,pw)) { // 로그인 성공
+            // ~~ 로그인시 이동할 다음페이지
+        }
+        else {
+            // sleep을 통해 endl 랑 \n 차이 비교
+            cout << "\n!! 아이디와 비밀번호를 다시 입력 해주세요.";
+            Sleep(3000); // 3초후 로그인페이지 호출
+            system("cls");
+            login();
+        }
+        
+    }
+    else if (selectnum == 2) {
+        system("cls");
+        findId();
+        system("cls");
+        login();
+    }
+    else if (selectnum == 3) {
+        system("cls");
+        makeId();
+        system("cls");
+        login();
+    }
+    else {
+        cout << "\n!! 번호를 다시 입력 해주세요.";
+        Sleep(3000); // 3초후 로그인페이지 호출
+        system("cls");
+        login();
+    }
+
+    return;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    login();
+    return 0;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
