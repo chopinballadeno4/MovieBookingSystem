0a764416748a4b67a531d629e049d0c9db111b8d
BaeSungMin <qo98333@naver.com>
2021-07-26 16:23:33 +0900

    로그인 70%

--- MovieBookingSystem.cpp
+++ MovieBookingSystem.cpp
@@ -1,13 +1,111 @@
 ﻿//#define endl '\n' 왜 오류가 나오는지 잘 모르겠음
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <stdio.h>
+//#include <stdio.h>
 #include <string>
+#include <vector>
 #include <Windows.h>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
-void makeId() {
+//-------------------------------------------------------------------------
+vector<user> userlist;
 
+class user {
+private:
+    string name;
+    string phonenum;
+    string email;
+    string id;
+    int pw;
+
+public:
+    user(string name, string phonenum, string email, string id, int pw) {
+        //this->name = name;
+        this->name = name;
+        this->phonenum = phonenum;
+        this->email = email;
+        this->id = id;
+        this->pw = pw;
+    }
+
+    ~user() {}
+
+    void setname(string name) {
+        this->name = name;
+    }
+    string getname() {
+        return name;
+    }
+
+    void setphonenum(string phonenum) {
+        this->phonenum = phonenum;
+    }
+    string getphonenum() {
+        return phonenum;
+    }
+
+    void setemail(string email) {
+        this->email = email;
+    }
+    string getemail() {
+        return email;
+    }
+
+    void setid(string id) {
+        this->id = id;
+    }
+    string getid() {
+        return id;
+    }
+
+    void setpw(int pw) {
+        this->pw = pw;
+    }
+    int getpw() {
+        return pw;
+    }
+};
+
+void makeuserlist() {
+    string filePath = "C:\\Users\\dnrtj\\Desktop\\프로젝트\\MovieBookingDB\\user.txt";
+    ifstream openFile(filePath.data());
+    if (openFile.is_open()) {
+        string line;
+        while (getline(openFile, line)) {
+            stringstream ss(line);
+            string name; ss >> name;
+            string phonenum; ss >> phonenum;
+            string email; ss >> email;
+            string id; ss >> id;
+            int pw; ss >> pw;
+            userlist.push_back(new user(name, phonenum, email, id, pw));
+        }
+        openFile.close();
+    }
+}
+
+
+
+//-------------------------------------------------------------------------
+
+
+void makeId() {
+    string name;
+    string phonenum;
+    string email;
+    string id;
+    int pw;
+
+    cout << "====== 회원 가입 =====" << "\n"; cout << "\n";
+    while(true) {
+        cout << "1. 이름 : "; cin >> name; cout << "\n";
+        cout << "2. 전화번호 : "; cin >> phonenum; cout << "\n";
+        cout << "3. email : "; cin >> email; cout << "\n";
+        cout << "4. id : "; cin >> id; cout << "\n";
+        cout << "5. pw : "; cin >> pw; cout << "\n";
+    }
 }
 
 void findId() {
@@ -29,46 +127,50 @@ void login() {
     cin >> selectnum;
     cout << 'k';
 
-    if (selectnum == 1) {
+    if (selectnum == 1) { // 로그인 선택시
         cout << '\n';
         cout << '\n';
         cout << '\n';
-        if (inputId(id,pw)) { // 로그인 성공
-            // ~~ 로그인시 이동할 다음페이지
+        if (inputId(id,pw)) { 
+            
         }
         else {
             // sleep을 통해 endl 랑 \n 차이 비교
             cout << "\n!! 아이디와 비밀번호를 다시 입력 해주세요.";
-            Sleep(3000); // 3초후 로그인페이지 호출
+            Sleep(3000); 
             system("cls");
             login();
         }
         
     }
-    else if (selectnum == 2) {
+    else if (selectnum == 2) { // ID/PW 찾기 선택시
         system("cls");
         findId();
+        Sleep(3000);
         system("cls");
         login();
     }
-    else if (selectnum == 3) {
+    else if (selectnum == 3) { // 회원가입 선택시
         system("cls");
         makeId();
+        Sleep(3000);
         system("cls");
         login();
     }
-    else {
+    else { // 엉뚱한 번호 선택시
         cout << "\n!! 번호를 다시 입력 해주세요.";
-        Sleep(3000); // 3초후 로그인페이지 호출
+        Sleep(3000);
         system("cls");
         login();
     }
-
     return;
 }
 
+//-------------------------------------------------------------------------
+
 int main()
 {
+    makeuserlist(); // 배열로 메모리 할당
     login();
     return 0;
 }
--- Study.cpp
+++ Study.cpp
@@ -4,7 +4,10 @@
    include하여 사용 가능. 주의 사항- cin>> 을통해 입력받을때 문자열의 경우 공백에서 구분이 되므로
    getline(cin,문자열변수) 를 통해서 문자열 전체를 입력받을 수 있음
 3. Windows.h 헤더파일을 가져온뒤 system("cls"); 를 사용하면 콘솔 화면을 초기화 할 수 있다.
-
-
-
+4. Person a1(10); 와 Person *a2 = new Person(10); 의 객체 생성 차이는
+   https://yoon90.tistory.com/13 해당 링크를 통해 공부했다
+5. struct vs class =
+6. 지역변수 , 전역변수 , static 변수 차이를 정리
+7. 파일 입출력 https://jdm.kr/blog/170
+8. c++ string split 하는 방법 https://www.acmicpc.net/board/view/25973 
 */
